adb6b8eb1e904ef889d9f9b59bc8d2f6b459074d
jacob.granberry <jacob.granberry@company-corp.com>
2023-07-27 18:14:39 -0500

    testing some content stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), markdoc()],
   experimental: {
-    assets: true,
-  },
+    assets: true
+  }
 });
--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -1,9 +1,27 @@
 // keystatic.config.ts
-import { config, fields, collection } from "@keystatic/core";
+import { config, fields, collection, singleton } from "@keystatic/core";
 
 export default config({
   storage: {
     kind: "local",
+  },
+  singletons: {
+    settings: singleton({
+      label: "Rookery Page",
+      schema: {
+        editions: fields.array(
+          fields.relationship({
+            label: "Editions",
+            description: "A list of past Rookery editions",
+            collection: "rookeries",
+          }),
+          {
+            label: "Rookeries",
+            itemLabel: (props) => props.value,
+          }
+        ),
+      },
+    }),
   },
   collections: {
     posts: collection({
@@ -27,7 +45,24 @@ export default config({
       slugField: "title",
       path: "src/content/rookeries/*",
       schema: {
-        title: fields.slug({ name: { label: "Title" } }),
+        title: fields.slug({
+          name: {
+            label: "Title",
+          },
+        }),
+        description: fields.text({ label: "Description" }),
+        link: fields.object({
+          url: fields.url({ label: "Link", validation: { isRequired: true } }),
+          isExternal: fields.checkbox({
+            label: "Is external link?",
+            defaultValue: true,
+          }),
+        }),
+        thumbnail: fields.image({
+          label: "Thumbnail",
+          description:
+            "Maximum upload file size: 12MB. Recommended file size for images is <500KB.",
+        }),
       },
     }),
   },
