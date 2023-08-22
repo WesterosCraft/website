e52329b7b32f31aec2554a3b6384a9e17f259321
jacob.granberry <jacobgranberry@gmail.com>
2023-08-21 20:36:14 -0500

    adds global search to data table

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -1,26 +1,30 @@
 import * as React from "react";
-import { Fragment, useEffect, useState } from "react";
 
 import {
   getCoreRowModel,
   useReactTable,
+  getPaginationRowModel,
+  getFilteredRowModel,
   flexRender,
 } from "@tanstack/react-table";
+import { rankItem } from "@tanstack/match-sorter-utils";
 
 import type {
+  Column,
   ColumnDef,
   Row,
   Table as ReactTable,
   PaginationState,
+  FilterFn,
 } from "@tanstack/react-table";
-import { cn } from "@lib/utils";
-import { Button } from "./ui/button";
-import {
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  ChevronsLeftIcon,
-  ChevronsRightIcon,
-} from "lucide-react";
+// import { cn } from "@lib/utils";
+// import { Button } from "./ui/button";
+// import {
+//   ChevronLeftIcon,
+//   ChevronRightIcon,
+//   ChevronsLeftIcon,
+//   ChevronsRightIcon,
+// } from "lucide-react";
 
 interface ReactTableProps<T extends object> {
   data: T[];
@@ -33,149 +37,248 @@ interface ReactTableProps<T extends object> {
   className?: string;
 }
 
-function Table<T extends object>({
-  data,
-  columns,
-  renderSubComponent,
-  pageIndex,
-  pageSize,
-  pageCount,
-  onPaginationChange,
-  className,
-}: ReactTableProps<T>) {
-  const [pagination, setPagination] = useState<PaginationState>({
-    pageIndex: pageIndex ?? 0,
-    pageSize: pageSize ?? 15,
+const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
+  // Rank the item
+  const itemRank = rankItem(row.getValue(columnId), value);
+
+  // Store the itemRank info
+  addMeta({
+    itemRank,
   });
+
+  // Return if the item should be filtered in/out
+  return itemRank.passed;
+};
+
+function Table<T extends object>({ data, columns }: ReactTableProps<T>) {
+  const [globalFilter, setGlobalFilter] = React.useState("");
 
   const table = useReactTable({
     data,
     columns,
-    getCoreRowModel: getCoreRowModel(),
-    manualPagination: true,
-    pageCount,
     state: {
-      pagination,
+      globalFilter,
     },
-    onPaginationChange: setPagination,
+    // Pipeline
+    onGlobalFilterChange: setGlobalFilter,
+    globalFilterFn: fuzzyFilter,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   });
 
-  useEffect(() => {
-    if (onPaginationChange) {
-      onPaginationChange(pagination);
-    }
-  }, [pagination, onPaginationChange]);
-
   return (
-    <div className="flex flex-col">
-      <div className="overflow-x-auto">
-        <div className="inline-block min-w-full py-1">
-          <div className="overflow-hidden p-1">
-            <table className={cn(`Table min-w-full`, className)}>
-              <thead>
-                {table.getHeaderGroups().map((headerGroup) => (
-                  <tr key={headerGroup.id}>
-                    {headerGroup.headers.map((header) => (
-                      <th key={header.id}>
-                        {header.isPlaceholder
-                          ? null
-                          : flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )}
-                      </th>
-                    ))}
-                  </tr>
-                ))}
-              </thead>
-
-              <tbody>
-                {table.getRowModel().rows.map((row) => (
-                  <Fragment key={row.id}>
-                    <tr
-                      className={cn({
-                        "bg-gray-50 transition-colors dark:bg-black-300":
-                          row.getIsExpanded(),
-                      })}
+    <div>
+      <div>
+        <DebouncedInput
+          value={globalFilter ?? ""}
+          onChange={(value) => setGlobalFilter(String(value))}
+          className='p-2 font-lg shadow border border-block'
+          placeholder='Search all columns...'
+        />
+      </div>
+      <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
+        <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
+          <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <th
+                      key={header.id}
+                      colSpan={header.colSpan}
+                      className='p-4'
                     >
-                      {row.getVisibleCells().map((cell) => (
-                        <td
-                          style={{
-                            width: cell.column.getSize(),
-                          }}
-                          key={cell.id}
-                        >
+                      {header.isPlaceholder ? null : (
+                        <div className='px-6 py-3'>
                           {flexRender(
-                            cell.column.columnDef.cell,
-                            cell.getContext()
+                            header.column.columnDef.header,
+                            header.getContext()
                           )}
-                        </td>
-                      ))}
-                    </tr>
-
-                    {renderSubComponent ? (
-                      <tr key={row.id + "-expanded"}>
-                        <td colSpan={columns.length}>
-                          {renderSubComponent({ row })}
-                        </td>
-                      </tr>
-                    ) : null}
-                  </Fragment>
-                ))}
-              </tbody>
-            </table>
-          </div>
-
-          <Pagination table={table} />
+                          {header.column.getCanFilter() ? (
+                            <div className='mt-4'>
+                              <Filter column={header.column} table={table} />
+                            </div>
+                          ) : null}
+                        </div>
+                      )}
+                    </th>
+                  );
+                })}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => {
+              return (
+                <tr
+                  key={row.id}
+                  className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+                >
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <td key={cell.id} className='px-6 py-4'>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <div className='h-2' />
+        <div className='flex items-center gap-2'>
+          <button
+            className='border rounded p-1'
+            onClick={() => table.setPageIndex(0)}
+            disabled={!table.getCanPreviousPage()}
+          >
+            {"<<"}
+          </button>
+          <button
+            className='border rounded p-1'
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            {"<"}
+          </button>
+          <button
+            className='border rounded p-1'
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            {">"}
+          </button>
+          <button
+            className='border rounded p-1'
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            disabled={!table.getCanNextPage()}
+          >
+            {">>"}
+          </button>
+          <span className='flex items-center gap-1'>
+            <div>Page</div>
+            <strong>
+              {table.getState().pagination.pageIndex + 1} of{" "}
+              {table.getPageCount()}
+            </strong>
+          </span>
+          <span className='flex items-center gap-1'>
+            | Go to page:
+            <input
+              type='number'
+              defaultValue={table.getState().pagination.pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                table.setPageIndex(page);
+              }}
+              className='border p-1 rounded w-16'
+            />
+          </span>
+          <select
+            value={table.getState().pagination.pageSize}
+            onChange={(e) => {
+              table.setPageSize(Number(e.target.value));
+            }}
+          >
+            {[10, 20, 30, 40, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </select>
         </div>
       </div>
     </div>
   );
 }
 
-function Pagination<T>({
+function Filter({
+  column,
   table,
-}: React.PropsWithChildren<{
-  table: ReactTable<T>;
-}>) {
-  return (
-    <div className="flex items-center gap-2">
-      <Button
-        onClick={() => table.setPageIndex(0)}
-        disabled={!table.getCanPreviousPage()}
-      >
-        <ChevronsLeftIcon className={"h-5"} />
-      </Button>
+}: {
+  column: Column<any, any>;
+  table: ReactTable<any>;
+}) {
+  const firstValue = table
+    .getPreFilteredRowModel()
+    .flatRows[0]?.getValue(column.id);
 
-      <Button
-        onClick={() => table.previousPage()}
-        disabled={!table.getCanPreviousPage()}
-      >
-        <ChevronLeftIcon className={"h-5"} />
-      </Button>
+  const columnFilterValue = column.getFilterValue();
 
-      <Button
-        onClick={() => table.nextPage()}
-        disabled={!table.getCanNextPage()}
-      >
-        <ChevronRightIcon className={"h-5"} />
-      </Button>
-
-      <Button
-        onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-        disabled={!table.getCanNextPage()}
-      >
-        <ChevronsRightIcon className={"h-5"} />
-      </Button>
-
-      <span className="flex items-center gap-1 text-sm">
-        <div>Page</div>
-
-        <strong>
-          {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
-        </strong>
-      </span>
+  return typeof firstValue === "number" ? (
+    <div className='flex space-x-2'>
+      <input
+        type='number'
+        value={(columnFilterValue as [number, number])?.[0] ?? ""}
+        onChange={(e) =>
+          column.setFilterValue((old: [number, number]) => [
+            e.target.value,
+            old?.[1],
+          ])
+        }
+        placeholder={`Min`}
+        className='w-24 border shadow rounded'
+      />
+      <input
+        type='number'
+        value={(columnFilterValue as [number, number])?.[1] ?? ""}
+        onChange={(e) =>
+          column.setFilterValue((old: [number, number]) => [
+            old?.[0],
+            e.target.value,
+          ])
+        }
+        placeholder={`Max`}
+        className='w-24 border shadow rounded'
+      />
     </div>
+  ) : (
+    <input
+      type='text'
+      value={(columnFilterValue ?? "") as string}
+      onChange={(e) => column.setFilterValue(e.target.value)}
+      placeholder={`Search...`}
+      className='w-36 border shadow rounded'
+    />
+  );
+}
+
+// A debounced input react component
+function DebouncedInput({
+  value: initialValue,
+  onChange,
+  debounce = 500,
+  ...props
+}: {
+  value: string | number;
+  onChange: (value: string | number) => void;
+  debounce?: number;
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange">) {
+  const [value, setValue] = React.useState(initialValue);
+
+  React.useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value);
+    }, debounce);
+
+    return () => clearTimeout(timeout);
+  }, [value]);
+
+  return (
+    <input
+      {...props}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
   );
 }
 
--- src/components/progress-table.tsx
+++ src/components/progress-table.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import type { ColumnDef } from "@tanstack/react-table";
 import Table from "./data-table";
 
 interface ProgressData {
@@ -13,37 +14,41 @@ interface Props {
 }
 
 export function ProgressTable({ data }: Props) {
-  const columns = [
-    {
-      id: "name",
-      header: "Name",
-      accessorKey: "title",
-      cell: (ctx) => ctx.getValue(),
-    },
-    {
-      id: "status",
-      header: "Status",
-      accessorKey: "projectStatus",
-      cell: (ctx) => ctx.getValue(),
-    },
-    {
-      id: "region",
-      header: "Region",
-      accessorKey: "region",
-      cell: (ctx) => ctx.getValue(),
-      //   cell: (ctx) => {
-      //     const { email } = ctx.row.original;
-
-      //     return <span className="font-bold">{email}</span>;
-      //   },
-    },
-    {
-      id: "type",
-      header: "Type",
-      accessorKey: "projectType",
-      cell: (ctx) => ctx.getValue(),
-    },
-  ];
+  const columns = React.useMemo<ColumnDef<ProgressData>[]>(
+    () => [
+      {
+        id: "name",
+        header: "Name",
+        accessorKey: "title",
+        cell: (ctx) => {
+          return (
+            <span className='font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+              {ctx.getValue() as React.ReactNode}
+            </span>
+          );
+        },
+      },
+      {
+        id: "status",
+        header: "Status",
+        accessorKey: "projectStatus",
+        cell: (ctx) => ctx.getValue(),
+      },
+      {
+        id: "region",
+        header: "Region",
+        accessorKey: "region",
+        cell: (ctx) => ctx.getValue(),
+      },
+      {
+        id: "type",
+        header: "Type",
+        accessorKey: "projectType",
+        cell: (ctx) => ctx.getValue(),
+      },
+    ],
+    []
+  );
 
   return <Table data={data} columns={columns} />;
 }
