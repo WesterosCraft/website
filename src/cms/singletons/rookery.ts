b93f14fd3393fdba206536af1d813467709a8384
jacob.granberry <jacobgranberry@gmail.com>
2023-10-23 18:25:14 -0500

    fix project status

--- src/cms/singletons/rookery.ts
+++ src/cms/singletons/rookery.ts
@@ -1,9 +1,16 @@
 import { fields, singleton } from "@keystatic/core";
+import { link } from "./../fields/links";
 
 export const rookery = singleton({
   label: "Rookery Page",
   path: "src/content/pages/rookery",
   schema: {
+    heading: fields.text({ label: "Heading" }),
+    subheading: fields.text({ label: "Subheading" }),
+    banner: fields.image({
+      label: "Banner",
+      directory: "src/assets/pages/rookery",
+    }),
     editions: fields.array(
       fields.object({
         title: fields.slug({
@@ -20,6 +27,7 @@ export const rookery = singleton({
         }),
         thumbnail: fields.image({
           label: "Thumbnail",
+          directory: "src/assets/pages/rookery/thumbnails",
           description:
             "Maximum upload file size: 12MB. Recommended file size for images is <500KB.",
         }),
@@ -29,6 +37,14 @@ export const rookery = singleton({
         description: "A list of all rookery editions",
         itemLabel: (props) => props?.fields?.title?.value?.name || "Edition",
       }
+    ),
+    cta: fields.object(
+      {
+        heading: fields.text({ label: "Heading" }),
+        subheading: fields.text({ label: "Subheading", multiline: true }),
+        link,
+      },
+      { label: "CTA" }
     ),
   },
 });
