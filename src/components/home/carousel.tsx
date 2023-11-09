3f70a7566a7609be1731b0077a19d9db8d513fe4
jacob.granberry <jacobgranberry@gmail.com>
2023-11-09 12:24:02 -0600

    some carousel updates for location pages

--- src/components/home/carousel.tsx
+++ src/components/home/carousel.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import clsx from "clsx";
 import { useKeenSlider } from "keen-slider/react";
+import { ExternalLink } from "lucide-react";
 import "keen-slider/keen-slider.min.css";
+import { Button } from "@components/ui/button";
 
 interface Slide {
   image: string;
   alt: string;
 }
 
-export default function Carousel({ slides }: { slides: Slide[] }) {
+interface CarouselProps {
+  slides: Slide[];
+  showDots?: boolean;
+  slideHeight?: number;
+  hideLeftArrow?: boolean;
+  linkImageExternally?: boolean;
+}
+
+export default function Carousel({
+  slides,
+  showDots,
+  slideHeight,
+  hideLeftArrow,
+  linkImageExternally,
+}: CarouselProps) {
   const [opacities, setOpacities] = React.useState<number[]>([]);
   const [loaded, setLoaded] = React.useState<boolean[]>([]);
   const [created, setCreated] = React.useState<boolean>(false);
@@ -47,53 +63,111 @@ export default function Carousel({ slides }: { slides: Slide[] }) {
   }, [currentSlide]);
 
   return (
-    <div ref={sliderRef} className='keen-slider relative'>
-      {slides.map((slide, idx) => (
-        <div key={idx} className='keen-slider__slide lazy__slide relative'>
-          <img
-            className={clsx(
-              "object-cover h-[var(--slideHeight)] bg-transparent",
-              loaded[idx] ? "bg-transparent" : "hidden"
+    <div>
+      <div ref={sliderRef} className='keen-slider relative'>
+        {slides?.map((slide, idx) => (
+          <div key={idx} className='keen-slider__slide lazy__slide relative'>
+            <img
+              className={clsx(
+                `object-cover bg-transparent`,
+                loaded[idx] ? "bg-transparent" : "hidden",
+                slideHeight ? `h-[${slideHeight}px]` : "h-[var(--slideHeight)]"
+              )}
+              sizes='60vw'
+              src={loaded[idx] ? slide.image : ""}
+              width={1100}
+              height={slideHeight ? `${slideHeight}px` : "var(--slideHeight)"}
+              alt={slide.alt || ""}
+              style={{ opacity: opacities[idx] }}
+            />
+            {slide?.alt && (
+              <p className='font-esmeralda absolute text-3xl bottom-4 left-4 text-white'>
+                {slide.alt || ""}
+              </p>
             )}
-            sizes='60vw'
-            src={loaded[idx] ? slide.image : ""}
-            width={1100}
-            height='var(--slideHeight)'
-            alt={slide.alt || ""}
-            style={{ opacity: opacities[idx] }}
-          />
-          <p className='font-esmeralda absolute text-3xl bottom-4 left-4 text-white'>
-            {slide.alt || ""}
-          </p>
-        </div>
-      ))}
-      {created && instanceRef.current && (
-        <div className='arrow-button absolute bottom-4 right-4 w-7 h-7 fill-white cursor-pointer'>
-          <Arrow
-            onClick={(e: any) =>
-              e.stopPropagation() || instanceRef.current?.next()
-            }
-            disabled={
-              currentSlide ===
-              instanceRef.current.track.details.slides.length - 1
-            }
-          />
+            {linkImageExternally && (
+              <div className='absolute top-2 right-2 fill-white'>
+                <a href={slide.image} target='_blank' rel='noreferrer'>
+                  <Button
+                    variant='ghost'
+                    className='p-2 rounded-full h-auto hover:bg-slate-50/50 text-white'
+                  >
+                    <ExternalLink />
+                  </Button>
+                </a>
+              </div>
+            )}
+          </div>
+        ))}
+        {created && instanceRef.current && (
+          <>
+            {!hideLeftArrow && (
+              <div className='arrow-button absolute bottom-4 left-4 w-7 h-7 fill-white cursor-pointer'>
+                <Arrow
+                  left
+                  onClick={(e: any) =>
+                    e.stopPropagation() || instanceRef.current?.prev()
+                  }
+                  disabled={currentSlide === 0}
+                />
+              </div>
+            )}
+            <div className='arrow-button absolute bottom-4 right-4 w-7 h-7 fill-white cursor-pointer'>
+              <Arrow
+                onClick={(e: any) =>
+                  e.stopPropagation() || instanceRef.current?.next()
+                }
+                disabled={
+                  currentSlide ===
+                  instanceRef.current.track.details.slides?.length - 1
+                }
+              />
+            </div>
+          </>
+        )}
+      </div>
+      {showDots && loaded && instanceRef.current && (
+        <div className='dots'>
+          {[
+            ...Array(instanceRef.current.track.details.slides.length).keys(),
+          ].map((idx) => {
+            return (
+              <button
+                key={idx}
+                onClick={() => {
+                  instanceRef.current?.moveToIdx(idx);
+                }}
+                className={"dot" + (currentSlide === idx ? " active" : "")}
+              ></button>
+            );
+          })}
         </div>
       )}
     </div>
   );
 }
 
-function Arrow(props: { disabled: boolean; onClick: (e: any) => void }) {
-  const disabeld = props.disabled ? " arrow--disabled" : "";
+function Arrow(props: {
+  disabled: boolean;
+  left?: boolean;
+  onClick: (e: any) => void;
+}) {
+  const disabeld = props.disabled ? "arrow--disabled" : "";
   return (
     <svg
       onClick={props.onClick}
-      className={`arrow arrow--right ${disabeld}`}
+      className={`arrow ${
+        props.left ? "left-[5px]" : "arrow--right"
+      } ${disabeld}`}
       xmlns='http://www.w3.org/2000/svg'
       viewBox='0 0 24 24'
     >
-      <path d='M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z' />
+      {props.left && (
+        <path d='M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z' />
+      )}
+      {!props.left && (
+        <path d='M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z' />
+      )}
     </svg>
   );
 }
