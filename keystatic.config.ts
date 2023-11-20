07454aba58af9774f23cdaf0fa80e91a29f28c0c
jacob.granberry <jacobgranberry@gmail.com>
2023-11-20 13:23:33 -0600

    update some guides

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,21 +1,16 @@
+import { config } from "@keystatic/core";
 import { footer } from "./src/cms/singletons/footer";
 import { mainnav } from "./src/cms/singletons/main-nav";
 import { join } from "./src/cms/singletons/join";
 import { about } from "./src/cms/singletons/about";
 import { banner } from "./src/cms/singletons/banner";
-import { config } from "@keystatic/core";
 import { locations, docs } from "./src/cms/collections";
 import { rookery, home } from "./src/cms/singletons";
 
 export default config({
   storage: {
     kind: import.meta.env.DEV ? "local" : "cloud",
-    // kind: import.meta.env.DEV ? "local" : "github",
   },
-  // repo: {
-  //   owner: "WesterosCraft",
-  //   name: "website",
-  // },
   cloud: {
     project: "westeroscraft/website",
   },
